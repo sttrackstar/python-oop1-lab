7aff83a3055d2bdf8c333102421ebbdc25b19393
sttrackstar <sonalt321@gmail.com>
2026-07-07 04:18:12 -0400

    Assignment completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.year})"
+
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, name, origin, roast):
+        self.name = name
+        self.origin = origin
+        self.roast = roast
+
+    def __str__(self):
+        return f"{self.name} from {self.origin} ({self.roast} roast)"
